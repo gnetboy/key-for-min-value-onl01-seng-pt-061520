0f4e8818f09555208699528d18c938d0795045c1
Gustavo Hernandez <gnetboy.y@gmail.com>
2020-06-29 06:18:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,12 @@
  
  
 def key_for_min_value(hash)
- 
+  lowest_key = hash.reduce { |key, value|key.last > value.last ? value : key}
+
+    if hash == {}
+      lowest_key
+    else
+      lowest_key.first
+    end
 end
 
